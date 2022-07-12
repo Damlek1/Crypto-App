f7f5623e542dfcc37849345415576a459a460dd7
Damlek1 <100087318+Damlek1@users.noreply.github.com>
2022-07-12 18:31:41 +0100

    commit

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -42,7 +42,7 @@ const Featured = () => {
                 </div>
                 <div>
                     <h5>{data[0].name}</h5>
-                    <p>{data[0].current_price.toLocaleString()}</p>
+                    <p>${data[0].current_price.toLocaleString()}</p>
                 </div>
                 {data[0].price_change_percentage_24h < 0 ? (
                     <span className='red'><FiArrowDown className='icon' />{data[0].price_change_percentage_24h.toFixed(2)}%</span>
@@ -59,7 +59,7 @@ const Featured = () => {
                 </div>
                 <div>
                     <h5>{data[1].name}</h5>
-                    <p>{data[1].current_price.toLocaleString()}</p>
+                    <p>${data[1].current_price.toLocaleString()}</p>
                 </div>
                 {data[1].price_change_percentage_24h < 0 ? (
                     <span className='red'><FiArrowDown className='icon' />{data[1].price_change_percentage_24h.toFixed(2)}%</span>
@@ -76,7 +76,7 @@ const Featured = () => {
                 </div>
                 <div>
                     <h5>{data[2].name}</h5>
-                    <p>{data[2].current_price.toLocaleString()}</p>
+                    <p>${data[2].current_price.toLocaleString()}</p>
                 </div>
                 {data[2].price_change_percentage_24h < 0 ? (
                     <span className='red'><FiArrowDown className='icon' />{data[2].price_change_percentage_24h.toFixed(2)}%</span>
@@ -93,7 +93,7 @@ const Featured = () => {
                 </div>
                 <div>
                     <h5>{data[3].name}</h5>
-                    <p>{data[3].current_price.toLocaleString()}</p>
+                    <p>${data[3].current_price.toLocaleString()}</p>
                 </div>
                 {data[3].price_change_percentage_24h < 0 ? (
                     <span className='red'><FiArrowDown className='icon' />{data[3].price_change_percentage_24h.toFixed(2)}%</span>
@@ -110,7 +110,7 @@ const Featured = () => {
                 </div>
                 <div>
                     <h5>{data[4].name}</h5>
-                    <p>{data[4].current_price.toLocaleString()}</p>
+                    <p>${data[4].current_price.toLocaleString()}</p>
                 </div>
                 {data[4].price_change_percentage_24h < 0 ? (
                     <span className='red'><FiArrowDown className='icon' />{data[4].price_change_percentage_24h.toFixed(2)}%</span>
@@ -127,7 +127,7 @@ const Featured = () => {
                 </div>
                 <div>
                     <h5>{data[5].name}</h5>
-                    <p>{data[5].current_price.toLocaleString()}</p>
+                    <p>${data[5].current_price.toLocaleString()}</p>
                 </div>
                 {data[5].price_change_percentage_24h < 0 ? (
                     <span className='red'><FiArrowDown className='icon' />{data[5].price_change_percentage_24h.toFixed(2)}%</span>
